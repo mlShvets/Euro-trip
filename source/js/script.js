eb0f7495d4b9a33a1249133d9bd2fc14e15a4ccd
Dmitrii Shvetsov <Shvec335@gmail.com>
2021-09-07 22:17:48 +0300

    Проверка линтером, слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -14,3 +14,37 @@ navToggle.addEventListener('click', () => {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+// slider
+
+const catalogMenuItem = document.querySelectorAll('.places__nav-btn');
+const catalogLink = document.querySelectorAll('.catalog__link');
+const countryTab = document.querySelectorAll('.places__item');
+
+
+const activeClassRemove = (collection, activeClass) => {
+  collection.forEach((element) => {
+    if (element.classList.contains(activeClass)) {
+      element.classList.remove(activeClass);
+    }
+  });
+};
+
+const catalogMenuSwitch = (index) => {
+  activeClassRemove(catalogMenuItem, "places__nav-btn--current");
+  activeClassRemove(countryTab, "places__item--current");
+  catalogMenuItem[index].classList.add("places__nav-btn--current");
+  countryTab[index].classList.add("places__item--current");
+};
+
+catalogMenuItem.forEach((element, index) => {
+  element.addEventListener('click', () => {
+    catalogMenuSwitch(index);
+  });
+});
+
+catalogLink.forEach((element, index) => {
+  element.addEventListener('click', () => {
+    catalogMenuSwitch(index);
+  });
+});
